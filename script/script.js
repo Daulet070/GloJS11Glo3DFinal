e61d4919d5533431f42a27b42b0b814414fcfd8f
Daulet <anahaev@outlook.com>
2020-02-21 05:17:16 +0300

    Lesson17 completed

--- script/script.js
+++ script/script.js
@@ -1,34 +1,34 @@
 window.addEventListener('DOMContentLoaded', function(){
   'use strict';
   // Timer
-  function countTimer(deadline){
-    let timerHours = document.querySelector('#timer-hours'),
-        timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+  const countTimer = (deadline) => {
+    const timerHours = document.querySelector('#timer-hours'),
+          timerMinutes = document.querySelector('#timer-minutes'),
+          timerSeconds = document.querySelector('#timer-seconds');
         
-    function getTimeRemaining(){
+    const getTimeRemaining = () => {
       let dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
           timeRemaining = (dateStop - dateNow) / 1000,
           seconds = Math.floor(timeRemaining % 60),
           minutes = Math.floor((timeRemaining / 60) % 60),
           hours = Math.floor(timeRemaining / 60 / 60);
+          if (seconds < 10) {seconds = '0' + seconds;}
+          if (minutes < 10) {minutes = '0' + minutes;}
+          if (hours < 10)   {hours = '0' + hours;}
           return {timeRemaining, hours, minutes, seconds};
     }
-    
-    function updateClock(){
-      let timer = getTimeRemaining();
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
+    const updateClock = () => {
+      const timer = getTimeRemaining();
 
       if(timer.timeRemaining > 0) {
 
-        setInterval(updateClock, 1000);
+        timerHours.textContent = timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        timerSeconds.textContent = timer.seconds;
 
       } else if (timer.timeRemaining < 0) {
-        
-        let intervalIndex = setInterval(updateClock, 1000);
+
         clearInterval(intervalIndex);
 
         timerHours.textContent = '00';
@@ -36,8 +36,7 @@ window.addEventListener('DOMContentLoaded', function(){
         timerSeconds.textContent = '00';
       }
     }
-    
-    updateClock();
+    const intervalIndex = setInterval(updateClock, 1000);
   }
   countTimer('21 February 2020');
 });
