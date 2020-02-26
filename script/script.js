0c5dfe9822aa86851ac1b121979c6a913fbd8ff8
Daulet <anahaev@outlook.com>
2020-02-26 21:44:10 +0300

    Lesson22 complete

--- script/script.js
+++ script/script.js
@@ -234,19 +234,65 @@ window.addEventListener('DOMContentLoaded', function(){
     });
   };
   commandPhoto();
-  // Calc
+
+  // Calc validation
   const getCalc = () => {
     const calcItem = document.querySelectorAll('[type="number"]');
 
     calcItem.forEach( elem => {
-      elem.addEventListener('input', (event) => {
+      elem.addEventListener('input', () => {
 
-        event.target.value = event.target.value.replace(/\D/, '');
-        console.log('event.target.value: ', event.target.value);
+        elem.value = elem.value.replace(/\D/, '');
+        console.log('elem.value: ', elem.value);
 
       });
     });
+    
   };
   getCalc();
+  
+  // Calc
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total');
+
+    const countSumm = () => {
+      let total = 0,
+          countValue = 1, 
+          dayValue = 1; 
+
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+
+      if(target.matches('select') || target.matches('input')){
+        countSumm();
+      }
+    });
+  }
+  calc(100);
 
 });
